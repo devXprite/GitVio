53528239c11a9cb74b25e3cfcd0c897f7af4734c
7ORP3DO <davisudha806@gmail.com>
2022-07-22 17:16:57 +0530

    create home page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'GitVio',
+    description: 'Generate beautiful portfolios from your GitHub profile'
+  });
 });
 
 module.exports = router;
